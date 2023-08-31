67df8b003bd5f1617e898399f5bfa6568be71eff
Alexandre Morgado <alexandre.correia.morgado@gmail.com>
2023-08-31 12:20:10 +0100

    Add await routines on routes

--- app/routes/api_v1.py
+++ app/routes/api_v1.py
@@ -11,10 +11,10 @@ twilio = TwilioHandler(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH
 @router.post("/twilio_voice", response_class=HTMLResponse)
 async def handle_twilio_voice():
     resp = VoiceResponse()
-    return twilio.greet_and_gather(resp)
+    return await twilio.greet_and_gather(resp)
 
 
 @router.post("/handle-speech", response_class=HTMLResponse)
 async def handle_speech():
     resp = VoiceResponse()
-    return twilio.handle_speech(resp)
+    return await twilio.handle_speech(resp)
