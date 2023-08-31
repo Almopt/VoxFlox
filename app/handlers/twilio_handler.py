504bb8056539bd57c85b9f037b270c754293824b
Alexandre Morgado <alexandre.correia.morgado@gmail.com>
2023-08-31 15:29:44 +0100

    Changed the endpoint names for something better. Added Request parameter on function handle_dialog to get the response from the answer call.

--- app/handlers/twilio_handler.py
+++ app/handlers/twilio_handler.py
@@ -6,20 +6,17 @@ class TwilioHandler:
         self.client = Client(account_sid, auth_token)
 
     def greet_and_gather(self, response):
-        with response.gather(input='speech', action='/v1/handle-speech', speech_model='experimental_conversations',
+        with response.gather(input='speech', action='/v1/handle-dialog', speech_model='experimental_conversations',
                              language='pt-PT') as gather:
             gather.say(message='Olá! Bem-vindo á Pizzaria Imaginária, em que posso ajudá-lo?', language='pt-PT')
 
         return str(response)
 
-    def handle_speech(self, response):
-        with response.gather(input='speech', action='/v1/handle-speech', speechTimeout='2',
+    def handle_dialog(self, response):
+        with response.gather(input='speech', action='/v1/handle-dialog', speechTimeout='2',
                              speech_model='experimental_conversations', method='POST', language='pt-PT') as gather:
             gather.say(message='Recebemos a sua mensagem com sucesso, para terminar a chamada diga adeus',
                        language='pt-PT')
-
-        # response.say('Se desejar, você pode finalizar a chamada a qualquer momento. Caso contrário, continue a falar.')
-        # response.record(timeout=5, action='/handle-record', method='POST')
 
         return str(response)
 
--- app/routes/api_v1.py
+++ app/routes/api_v1.py
@@ -3,18 +3,26 @@ from fastapi.responses import HTMLResponse
 from twilio.twiml.voice_response import VoiceResponse
 import os
 from ..handlers.twilio_handler import TwilioHandler
+from starlette.requests import Request
+from urllib.parse import parse_qs
 
 router = APIRouter()
 twilio = TwilioHandler(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
 
 
-@router.post("/twilio_voice", response_class=HTMLResponse)
-def handle_twilio_voice():
+@router.post("/answer_call", response_class=HTMLResponse)
+def answer_call():
     resp = VoiceResponse()
     return twilio.greet_and_gather(resp)
 
 
-@router.post("/handle-speech", response_class=HTMLResponse)
-def handle_speech():
+@router.post("/handle-dialog", response_class=HTMLResponse)
+async def handle_dialog(request: Request):
+    body = await request.body()
+    print(body.decode())
+    data_dict = parse_qs(body.decode())
+    print(data_dict.get('SpeechResult', ['']))
+    print(data_dict.get('Confidence', ['']))
+
     resp = VoiceResponse()
-    return twilio.handle_speech(resp)
+    return twilio.handle_dialog(resp)
