114ac729a801d4d0ab22c08414364ce392cce20d
Alexandre Morgado <alexandre.correia.morgado@gmail.com>
2023-09-06 11:35:35 +0100

    added data file with menu and some other changes

--- app/handlers/langchain_handler.py
+++ app/handlers/langchain_handler.py
@@ -1,4 +1,31 @@
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationSummaryMemory
 
 class LangChainHandler:
     def __init__(self, openai_api_key):
-        print('bye')
+        self.__llm = ChatOpenAI(temperature='0.2', openai_api_key=openai_api_key)
+        self.__memory = ConversationSummaryMemory(llm=self.__llm)
+        self.__template = """
+            You are a nice and professional assistance for restaurants. I will share the restaurant menu and all
+            his information and you will give me the best answer that will help the costumer make a order or a 
+            reservation. You will follow all rules bellow:
+            
+            1/Understand the purpose of the call, whether it's to place a takeaway order, 
+            delivery or to make a reservation.
+            
+            2/ If it's a take-away order, help with any questions the customer may have about the menu. 
+            Ask what the order is under and thank them for their preference.
+            
+            3/If it's a home delivery order, help with any questions the customer may have about the menu. 
+            Ask for the delivery address and thank them for their preference.
+            
+            4/If it's a reservation, ask how many people the reservation is for and what name the reservation is in.
+            
+            Bellow is the menu of the restaurant:
+            {rest_menu}
+            
+            Please answer nicely.
+        """
--- app/routes/api_v1.py
+++ app/routes/api_v1.py
@@ -3,23 +3,29 @@ from fastapi.responses import HTMLResponse
 from twilio.twiml.voice_response import VoiceResponse
 import os
 from ..handlers.twilio_handler import TwilioHandler
+from ..handlers.langchain_handler import LangChainHandler
 from starlette.requests import Request
 from urllib.parse import parse_qs
 
 router = APIRouter()
 twilio = TwilioHandler(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+langchain = LangChainHandler(os.environ['OPENAI_API_KEY'])
 
 
 @router.post("/answer_call", response_class=HTMLResponse)
-def answer_call():
+async def answer_call(request: Request):
     resp = VoiceResponse()
+    twilio_signature = request.headers
+    print(twilio_signature)
+    form_test = await request.form()
+    print(form_test)
     return twilio.greet_and_gather(resp)
 
 
 @router.post("/handle-dialog", response_class=HTMLResponse)
 async def handle_dialog(request: Request):
     body = await request.body()
-    #print(body.decode())
+    print(body.decode())
     data_dict = parse_qs(body.decode())
     print(data_dict.get('SpeechResult', ['']))
     #print(data_dict.get('Confidence', ['']))
