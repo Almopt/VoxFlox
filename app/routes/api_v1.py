df32ede24e2350854a70f132806790adf84e4eec
Alexandre Morgado <alexandre.correia.morgado@gmail.com>
2023-09-07 10:04:06 +0100

    Trying to catch error 500 server v24

--- app/routes/api_v1.py
+++ app/routes/api_v1.py
@@ -6,6 +6,7 @@ from ..handlers.twilio_handler import TwilioHandler, TwilioValidationException
 from ..handlers.langchain_handler import LangChainHandler
 from starlette.requests import Request
 from urllib.parse import parse_qs
+from collections import OrderedDict
 
 router = APIRouter()
 twilio = TwilioHandler(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
@@ -22,6 +23,27 @@ async def answer_call(request: Request):
         signature = request.headers.get('X-Twilio-Signature')
         print(f'Signature - {signature}')
         request_form = await request.form()
+
+        # The data in string format
+        data_str = str(request_form)
+
+        # Extract the content within parentheses and split it by commas
+        items = data_str[data_str.find("(") + 1:data_str.rfind(")")].split(",")
+
+        # Initialize an empty dictionary
+        data_dict = {}
+
+        # Loop through the items and split them into key-value pairs
+        for item in items:
+            key, value = item.strip(" '()").split(", ")
+            data_dict[key] = value
+
+        # Sort the dictionary by keys and create an ordered dictionary
+        sorted_dict = OrderedDict(sorted(data_dict.items(), key=lambda x: x[0]))
+
+        print(sorted_dict)
+
+
         print(request_form)
         #form_data = {key: value.strip('[]') for key, value in sorted(request_form)}
         form_data = {key: value for key, value in sorted(request_form)}
