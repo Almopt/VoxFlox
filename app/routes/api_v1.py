fb1c0c59a4807f2515dd12fa775d52293e32a662
Alexandre Morgado <alexandre.correia.morgado@gmail.com>
2023-09-12 16:56:07 +0100

    Understanding **kwargs from gather method from twilio v08

--- app/routes/api_v1.py
+++ app/routes/api_v1.py
@@ -73,23 +73,22 @@ async def answer_call(request: Request):
 @router.post("/handle-dialog", response_class=HTMLResponse)
 async def handle_dialog(request: Request):
     body = await request.body()
-    request_form = await request.form()
+    #request_form = await request.form()
 
-    query = request.query_params
-    print(query)
-    print(query.get('cv_id'))
+    query_params = request.query_params
+    print(query_params.get('cv_id'))
 
-    print(request.headers)
+    #print(request.headers)
 
-    print(request.items())
+    #print(request.items())
 
-    print(request.values())
-    print(request.query_params)
+    #print(request.values())
+    #print(request.query_params)
     #print(dict(request.query_params))
     print(body.decode())
     data_dict = parse_qs(body.decode())
     print(data_dict.get('SpeechResult', ['']))
-    print(request_form)
+    #print(request_form)
     #print(data_dict.get('Confidence', ['']))
 
     resp = VoiceResponse()
